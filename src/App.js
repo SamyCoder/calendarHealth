d9e235123ce24ee8965e622437a255853e8feee7
Samridhi Roshan <samridhi.roshan2@gmail.com>
2023-11-20 19:08:57 -0600

    adding the spotify bar and fixed FE

--- src/App.js
+++ src/App.js
@@ -9,17 +9,6 @@ import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import "./App.css";
 
-// utils.js
-
-function getRandomColor() {
-    const letters = "0123456789ABCDEF";
-    let color = "#";
-    for (let i = 0; i < 6; i++) {
-        color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-}
-
 const locales = {
     "en-US": require("date-fns/locale/en-US"),
 };
@@ -50,6 +39,157 @@ const events = [
     },
 ];
 
+
+const LegendButton = () => {
+    const [showLegend, setShowLegend] = useState(false);
+
+    const legendContent = [
+        { color: 'rgb(0, 0, 255)', text: 'Less than 55' },
+        { color: 'rgb(255, 255, 0)', text: '55 to 85' },
+        { color: 'rgb(0, 255, 0)', text: '85 to 116' },
+        { color: 'rgb(255, 0, 0)', text: 'More than 116' },
+        // Add more legend items as needed
+    ];
+
+    return (
+        <div className="legend-container">
+            <button className="legend-toggle" onClick={() => setShowLegend(!showLegend)}>
+                Legend
+            </button>
+
+            {showLegend && (
+                <div className="legend-popup">
+                    {legendContent.map((item, index) => (
+                        <div key={index} className="legend-item">
+                            <span className="legend-color-box" style={{ backgroundColor: item.color }}></span>
+                            <span className="legend-text">{item.text}</span>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+const AnalyticsButton = () => {
+    const [showLegend, setShowAnalytics] = useState(false);
+
+    const analyticsContent = [
+        { text: 'Showing some events to stress mappings,music to events to stress mappings' }
+        // Add more legend items as needed
+    ];
+
+    return (
+        <div className="analytics-container">
+            <button className="legend-toggle" onClick={() => setShowAnalytics(!showLegend)}>
+                Analytics
+            </button>
+
+            {showLegend && (
+                <div className="legend-popup">
+                    {analyticsContent.map((item, index) => (
+                        <div key={index} className="legend-item">
+                            <span className="legend-text">{item.text}</span>
+                        </div>
+                    ))}
+                </div>
+            )}
+        </div>
+    );
+};
+
+function valueToColor(value) {
+    // Define the value thresholds for your color scale
+    const lowThreshold = 60;  // Replace with your actual low threshold
+    const highThreshold = 120; // Replace with your actual high threshold
+
+    // Calculate the ratio of where the value falls between the low and high threshold
+    let ratio = (value - lowThreshold) / (highThreshold - lowThreshold);
+
+    // Define your colors for the lowest and highest values and for mid-range values
+    const lowColor = { r: 0, g: 0, b: 255 }; // Blue color for low values
+    const highColor = { r: 255, g: 0, b: 0 }; // Red color for high values
+
+    // You can define more colors for a more complex gradient
+    const midColor1 = { r: 255, g: 255, b: 0 }; // Yellow
+    const midColor2 = { r: 0, g: 255, b: 0 }; // Green
+
+    // Determine which two colors to use for interpolation
+    let startColor, endColor;
+    if (ratio < 0.5) {
+        startColor = lowColor;
+        endColor = midColor2;
+        // Adjust ratio to work with the new range
+        ratio = ratio * 2;
+    } else {
+        startColor = midColor1;
+        endColor = highColor;
+        // Adjust ratio to work with the new range
+        ratio = (ratio - 0.5) * 2;
+    }
+
+    // Interpolate between the two colors
+    const r = Math.round(startColor.r + (endColor.r - startColor.r) * ratio);
+    const g = Math.round(startColor.g + (endColor.g - startColor.g) * ratio);
+    const b = Math.round(startColor.b + (endColor.b - startColor.b) * ratio);
+
+    return `rgb(${r},${g},${b})`;
+}
+
+const hourValues = {
+    0: 72,
+    1: 77,
+    2: 77,
+    3: 80,
+    4: 80,
+    5: 75,
+    6: 100,
+    7: 70,
+    8: 116,
+    9: 85,
+    10: 75,
+    11: 73,
+    12: 77,
+    13: 83,
+    14: 81,
+    15: 80,
+    16: 72,
+    17: 94,
+    18: 110,
+    19: 100,
+    20: 75,
+    21: 78
+};
+
+const musicStatusColors = {
+    0: 'white',
+    1: 'white',
+    2: 'white',
+    3: 'white',
+    4: 'white',
+    5: 'white',
+    6: 'white',
+    7: 'green',
+    8: 'white',
+    9: 'white',
+    10: 'green',
+    11: 'green',
+    12: 'green',
+    13: 'white',
+    14: 'white',
+    15: 'green',
+    16: 'white',
+    17: 'white',
+    18: 'green',
+    19: 'white',
+    20: 'green',
+    21: 'white',
+    22: 'green',
+    23: 'white',
+    24: 'white',
+};
+
+
 function App() {
     const [newEvent, setNewEvent] = useState({ title: "", start: new Date(), end: new Date(), allDay: false });
     const [allEvents, setAllEvents] = useState(events);
@@ -57,7 +197,7 @@ function App() {
     function eventPropGetter(event, start, end, isSelected) {
         const style = {
             backgroundColor: 'rgba(0, 121, 191, 0.4)',  // This is the default event color with 60% opacity
-            marginLeft: '28px', //NOTE: Can change the marginLeft to Add Spotify item + reduce the width of the bar
+            marginLeft: '28px',
         };
         return {
             style: style
@@ -73,29 +213,42 @@ function App() {
 
             // Generate random colors for each hour of the day
             for (let hour = 0; hour < 24; hour++) {
-                hourColors[hour] = getRandomColor(); // Use getRandomColor or any other color generation logic
+                let color;
+                if (hourValues.hasOwnProperty(hour)) {
+                    // If the hour exists in hourValues, use its value for color
+                    const value = hourValues[hour];
+                    color = valueToColor(value);
+                } else {
+                    // If the hour doesn't exist in hourValues, use a default color
+                    color = 'white'; // Replace with any default color you prefer
+                }
+                hourColors[hour] = color;
             }
 
-            // Create a comma-separated list of colors for the repeating gradient
-            const colorList = Object.values(hourColors).join(", ");
+            const colorList1 = Object.values(hourColors);
+            const colorList2 = Object.values(musicStatusColors);
 
-            // Set the background style for the day cell
+            // Function to create a linear gradient for a single color
+            const createLinearGradient = (colors) => `linear-gradient(${colors.join(', ')})`;
+
+            // Create linear gradients for each color list
+            const gradient1 = createLinearGradient(colorList1);
+            const gradient2 = createLinearGradient(colorList2);
+
+            // Setting the background style for the day cell for 2 colors
             dayProps.style = {
-                backgroundImage: `linear-gradient(${colorList})`,
-                backgroundSize: "30px 100%", // Adjust the width and height of the strip
+                backgroundImage: `${gradient1}, ${gradient2}`,
+                backgroundSize: "30px 100%",
                 backgroundRepeat: "no-repeat",
-
+                backgroundPosition: "0 0, 35px 0",
+                position: 'relative',
             };
+
         }
 
         return dayProps;
-    }
-
-
-    function handleSpotifyButton(timestamp){
 
     }
-
 
     function handleSelectSlot(slotInfo) {
         const title = window.prompt('Please enter event name');
@@ -125,12 +278,6 @@ function App() {
             const d2 = new Date(newEvent.start);
             const d3 = new Date(allEvents[i].end);
             const d4 = new Date(newEvent.end);
-            /*
-                console.log(d1 <= d2);
-                console.log(d2 <= d3);
-                console.log(d1 <= d4);
-                console.log(d4 <= d3);
-                  */
 
             if (
                 ((d1 <= d2) && (d2 <= d3)) || ((d1 <= d4) &&
@@ -157,26 +304,6 @@ function App() {
                 <button stlye={{ marginTop: "10px" }} onClick={handleAddEvent}>
                     Add Event
                 </button>
-                {/* Spotify button Element*/}
-                <button
-                    style={{
-                        position: 'absolute',
-                        top: '10px',
-                        right: '10px', 
-                        zIndex: 1,  // For the button to be above other content
-                        borderRadius: '50%',
-                        backgroundColor: '#1DB954',
-                        border: 'none',
-                        cursor: 'pointer',
-                    }}
-                    onClick={handleSpotifyButton()} //NOTE: See how to get time/date pass, then use timestamp to access song info
-                >
-                    {/* <div dangerouslySetInnerHTML={{ __html: spotifyLogoSvg }} /> */}
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="17" fill="currentColor" class="bi bi-spotify" viewBox="0 0 16 16">
-                        <path d="M8 0a8 8 0 1 0 0 16A8 8 0 0 0 8 0zm3.669 11.538a.498.498 0 0 1-.686.165c-1.879-1.147-4.243-1.407-7.028-.77a.499.499 0 0 1-.222-.973c3.048-.696 5.662-.397 7.77.892a.5.5 0 0 1 .166.686zm.979-2.178a.624.624 0 0 1-.858.205c-2.15-1.321-5.428-1.704-7.972-.932a.625.625 0 0 1-.362-1.194c2.905-.881 6.517-.454 8.986 1.063a.624.624 0 0 1 .206.858zm.084-2.268C10.154 5.56 5.9 5.419 3.438 6.166a.748.748 0 1 1-.434-1.432c2.825-.857 7.523-.692 10.492 1.07a.747.747 0 1 1-.764 1.288z" />
-                    </svg>
-                </button>
-
             </div>
             <Calendar
                 localizer={localizer}
@@ -192,7 +319,14 @@ function App() {
                 selectable={true}  // make the calendar selectable
                 onSelectSlot={handleSelectSlot}
                 onSelectEvent={handleSelectEvent}
+            // components={{
+            //     dateCellWrapper: DateCellWrapper // Here we are passing our custom wrapper component
+            //   }}
             />
+            <div className="control-panel">
+                <LegendButton /> {/* This is the legend button */}
+                <AnalyticsButton /> {/* This is the analytics button */}
+            </div>
         </div>
     );
 }
